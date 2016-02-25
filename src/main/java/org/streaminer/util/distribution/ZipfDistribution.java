9cd3c792828407d2f57ae5b7acae4e637b6643f3
Michael <lwhaymail@yahoo.com>
2016-02-25 03:36:04 -0800

    Add a data generator

--- src/main/java/org/streaminer/util/distribution/ZipfDistribution.java
+++ src/main/java/org/streaminer/util/distribution/ZipfDistribution.java
@@ -3,7 +3,6 @@ package org.streaminer.util.distribution;
 import java.util.Random;
 
 /**
- *
  * @author Maycon Viana Bordin <mayconbordin@gmail.com>
  */
 public class ZipfDistribution {
@@ -13,38 +12,41 @@ public class ZipfDistribution {
     private double zetan;
     private long n;
     private Random random = new Random();
-    
+
     public ZipfDistribution(double theta, long n) {
         this(theta, n, n);
     }
-    
+
     public ZipfDistribution(double theta, long n, long length) {
         this.theta = theta;
         this.n = n;
-        
+
         alpha = 1. / (1. - theta);
         zetan = zeta(length, theta);
-	eta = (1. - Math.pow(2.0/n, 1.0 - theta)) / (1.0 - zeta(2, theta)/zetan);
+        eta = (1. - Math.pow(2.0 / n, 1.0 - theta)) / (1.0 - zeta(2, theta) / zetan);
     }
-    
+
     public double nextDouble() {
         double val;
         double u = random.nextDouble();
-	double uz = u * zetan;
-        
-	if (uz < 1) val = 1;
-	else if (uz < (1. + Math.pow(0.5, theta))) val = 2;
-	else val = 1 + (long)(n * Math.pow(eta*u - eta + 1.0, alpha));
+        double uz = u * zetan;
 
-	return val;
+        if (uz < 1)
+            val = 1;
+        else if (uz < (1. + Math.pow(0.5, theta)))
+            val = 2;
+        else
+            val = 1 + (long) (n * Math.pow(eta * u - eta + 1.0, alpha));
+
+        return val;
     }
-    
-    public static double zeta(long n, double theta)  {
-        double ans=0.0;
 
-        for (int i=1; i <= n; i++)
-            ans += Math.pow(1./(double)i, theta);
-        
+    public static double zeta(long n, double theta) {
+        double ans = 0.0;
+
+        for (int i = 1; i <= n; i++)
+            ans += Math.pow(1. / (double) i, theta);
+
         return ans;
     }
 }
